4ff2e1984e67ea14bad855c7474b381193dba512
rorymurphy <rorymurphy@users.noreply.github.com>
2020-05-07 22:54:09 -0400

    Added support for SCSS maps

--- src/compilers/Scss_Compiler.php
+++ src/compilers/Scss_Compiler.php
@@ -36,11 +36,47 @@ class Scss_Compiler {
         usort($vars, function($a, $b) use($config) {
             $aVar = $config->variables[$a['name']];
             $bVar = $config->variables[$b['name']];
-            return $aVar->order - $bVar->order;
+            if($aVar->order === $bVar->order) {
+                if($aVar->name == $bVar->name) {
+                    return 0;
+                }else {
+                    return ($aVar->name > $bVar->name) ? 1 : -1;
+                }
+            } else {
+                return $aVar->order - $bVar->order;
+            }
         });
 
+        $curr_map_name = null;
+        $curr_mappings = array();
         foreach($vars as $v) {
-            $variables_file .= '$' . $v['name'] . ': ' . $v['value'] . ";\n";
+            $matches = array();
+            $pattern = '/([\w\-\_]+)(?:\(([\w\-\_]+)\))?/';
+
+            preg_match($pattern, $v['name'], $matches);
+            //var_dump($matches);
+            $is_map = count($matches) == 3;
+            $map_name = $is_map ? $matches[1] : null;
+            $map_key = $is_map ? $matches[2] : null;
+
+            if($curr_map_name !== $map_name && $curr_map_name !== null) {
+                $variables_file .= $this->get_map_css($curr_map_name, $curr_mappings);
+                $curr_mappings = null;
+            }
+
+            if($is_map && $map_name !== $curr_map_name) {
+                $curr_mappings = $map_name;
+                $curr_mappings = array();
+                $curr_mappings[$map_key] = $v['value'];
+            } elseif($is_map) {
+                $curr_mappings[$map_key] = $v['value'];
+            } else {
+                $variables_file .= $this->get_variable_css($v['name'], $v['value']);
+            }
+        }
+        //In case we ended with a map, have to flush the last one
+        if($curr_map_name !== $map_name && $curr_map_name !== null) {
+            $variables_file .= $this->get_map_css($curr_map_name, $curr_mappings);
         }
 
         $compiler = new Compiler();
@@ -58,5 +94,21 @@ class Scss_Compiler {
         }
 
 
+    }
+
+    private function get_variable_css($name, $value) {
+        return '$' . $name . ': ' . $value . ";\n";
+    }
+
+    private function get_map_css($map_name, $mappings) {
+        $result = sprintf('$%1$s: (\n', $map_name);
+
+        $i = 0;
+        foreach($mappings as $key => $value) {
+            $is_last = ++$i == count($mappings);
+            $result .= sprintf('"%1$s": %2$s%3$s\n', $key, $value, $is_last ? ',' : '');
+        }
+        $result .= ');\n';
+        return $result;
     }
 }
